b539986c71ea7cdfdd1b9cdbc66901de2c6264ab
Dayne Game <daynegame024@gmail.com>
2017-09-04 10:33:26 +1200

    Task 1 done

--- Program.cs
+++ Program.cs
@@ -6,7 +6,44 @@ namespace problem1_csharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Start the program with Clear();
+            Console.Clear();
+            
+            Console.WriteLine("Welcome to the greater than project");
+
+            var number1 = 0;
+            var number2 = 0;
+
+            Console.WriteLine("Enter a number:");
+            var number1input = int.TryParse(Console.ReadLine(), out number1);
+
+            Console.WriteLine("Enter a number:");
+            var number2input = int.TryParse(Console.ReadLine(), out number2);
+
+            if (number1 > number2)
+            {
+                Console.WriteLine("Number 1 is greater than Number 2");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Number 2 is greater than number 1");
+                Console.ReadLine();
+            }
+            
+            //End the program with blank line and instructions
+            Console.ResetColor();
+            Console.WriteLine();
+            Console.WriteLine("Press <Enter> to quit the program");
+            Console.ReadKey();
+            
+            
+
+            
+
+            
+            
+            
         }
     }
 }
